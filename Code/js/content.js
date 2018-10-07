012f3dcdf94d800fcadb89bd55541aefcd17876e
Maxime Princelle <princellemaxime@gmail.com>
2018-10-07 19:17:33 +0200

    Adding Login interface

--- Code/js/content.js
+++ Code/js/content.js
@@ -1,11 +1,3 @@
-$(window).load(function() {
+/*$(window).load(function() {
 	$(".preloader").fadeOut("1000");
-})
-
-$(document).ready(function() {
-  $('#signin').hide();
-});
-
-$('#login').on('click',function(){
-    $('#signin').show();
-});
+})*/
