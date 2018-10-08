14aa89d3321d3b7b028ee133d8307f9540ff00db
Maxime Princelle <princellemaxime@gmail.com>
2018-10-08 10:12:17 +0200

    Preloader finish

--- Code/js/loading.js
+++ Code/js/loading.js
@@ -1,21 +1,5 @@
-$(".loading").height($(window).height());
-$(".loading").width($(window).width());
 
-
-$(".loading img").css({
-    paddingTop: ($(".loading").height() - $(".loading img").height()) / 2,
-    paddingLeft: ($(".loading").width() - $(".loading img").width()) / 2
-});
-
-$(window).resize(function () {
-    "use strict";
-
-    $(".loading").height($(window).height());
-    $(".loading").width($(window).width());
-
-    $(".loading img").css({
-        paddingTop: ($(".loading").height() - $(".loading img").height()) / 2,
-        paddingLeft: ($(".loading").width() - $(".loading img").width()) / 2
-    });
-
-});
+$(document).ready(function()
+{
+    $('#loader').fadeOut(4000);
+})
