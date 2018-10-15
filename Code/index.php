2713dbc9df38b9cc418b92968269d0d390e15599
Maxime Princelle <princellemaxime@gmail.com>
2018-10-15 16:44:35 +0200

    Finalize index.php

--- Code/index.php
+++ Code/index.php
@@ -61,11 +61,11 @@
                         <button class="nav-link dropdown-toggle btn btn-outline-secondary" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Jeux</button>
                         <div class="dropdown-menu shadow-lg" aria-labelledby="navbarDropdown">
                             <a class="dropdown-item" href="./quizz.html">Quizz</a>
-                            <a class="dropdown-item" href="./local.html">Jeu local</a>
+                            <a class="dropdown-item" href="./local.html">Solo</a>
                             <div class="dropdown-divider"></div>
                             <h6 class="dropdown-header">Multi-joueur</h6>
-                            <a class="dropdown-item" href="./local1v1.html">Local 1v1</a>
-                            <a class="dropdown-item" href="./lan.html">LAN 1v1</a>
+                            <a class="dropdown-item" href="./local1v1.html">OnSite - 1v1</a>
+                            <a class="dropdown-item" href="./lan.html">Réseau - 1v1</a>
                         </div>
                     </li>
                 </ul>
@@ -76,44 +76,45 @@
             <?php
                 if ($_SERVER['REQUEST_METHOD'] != 'GET' || !isset($_SESSION['user']))
                 {
-                    ?>
-                    <div class="nav-item navbar-right" id="login-interface">
-                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Connectez-vous
-                        </button>
-                        <div class="dropdown-menu dropdown-menu-right shadow-lg" style="position:absolute;" aria-labelledby="navbarDropdown">
-                          <form class="px-4 py-3 form-signin" method="POST" action="./php/authenticate.php">
-                            <div class="form-group">
-                                <label for="login">Votre email</label>
-                                <input type="email" id="login" name="login" class="form-control border" placeholder="Adresse Mail" required autofocus>
-                                <small id="emailHelp" class="form-text text-muted">Nous n'allons jamais partager votre adresse mail.</small>
+            ?>
+                <div class="nav-item navbar-right" id="login-interface">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Connectez-vous
+                    </button>
+                    <div class="dropdown-menu dropdown-menu-right shadow-lg" style="position:absolute;" aria-labelledby="navbarDropdown">
+                      <form class="px-4 py-3 form-signin" method="POST" action="./php/authenticate.php">
+                        <div class="form-group">
+                            <label for="login">Votre email</label>
+                            <input type="email" id="login" name="login" class="form-control border" placeholder="Adresse Mail" required autofocus>
+                            <small id="emailHelp" class="form-text text-muted">Nous n'allons jamais partager votre adresse mail.</small>
 
-                              <label for="password">Votre mot de passe</label>
-                              <input type="password" id="password" name="password" class="form-control border" placeholder="Mot de passe" required>
-                              <small id="passHelp" class="form-text text-muted">Attention ! Pour le moment ce site n'est pas très sécurisé.</small>
+                          <label for="password">Votre mot de passe</label>
+                          <input type="password" id="password" name="password" class="form-control border" placeholder="Mot de passe" required>
+                          <small id="passHelp" class="form-text text-muted">Attention ! Pour le moment ce site n'est pas très sécurisé.</small>
 
-                        </div>
-                        <button type="submit" class="btn btn-success no-action">Se connecter</button>
-                          </form>
-
-                          <div class="dropdown-divider"></div>
-                              <a class="dropdown-item" href="./signup.php">Nouveau ici ? Inscrivez-vous !</a>
-                              <a class="dropdown-item disabled" href="#">Vous avez oublié votre mot de passe ?</a>
-                        </div>
                     </div>
-                    <?php
+                    <button type="submit" class="btn btn-success no-action">Se connecter</button>
+                      </form>
+
+                      <div class="dropdown-divider"></div>
+                          <a class="dropdown-item" href="./signup.php">Nouveau ici ? Inscrivez-vous !</a>
+                          <a class="dropdown-item disabled" href="#">Vous avez oublié votre mot de passe ?</a>
+                    </div>
+                </div>
+            <?php
                 }
                 else {
-                    ?><div class="nav-item navbar-right" id="logged-interface">
-                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?= $_SESSION['user']; ?>
-                        </button>
-                        <div class="dropdown-menu dropdown-menu-right shadow-lg" style="position:absolute;" aria-labelledby="navbarDropdown">
+            ?>
+                <div class="nav-item navbar-right" id="logged-interface">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?= $_SESSION['user']; ?>
+                    </button>
+                    <div class="dropdown-menu dropdown-menu-right shadow-lg" style="position:absolute;" aria-labelledby="navbarDropdown">
 
-                          <button class="btn btn-danger">Se déconnecter</button>
-                          <div class="dropdown-divider"></div>
-                              <a class="dropdown-item disabled" href="#">Votre profil</a>
-                        </div>
+                      <button class="btn btn-danger">Se déconnecter</button>
+                      <div class="dropdown-divider"></div>
+                          <a class="dropdown-item disabled" href="#">Votre profil</a>
                     </div>
-                    <?php
+                </div>
+            <?php
                 }
             ?>
 
