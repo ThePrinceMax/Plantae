d07c0a621a12fd9b385179a3f1d7e35a7d887691
Maxime Princelle <princellemaxime@gmail.com>
2018-11-09 10:53:49 +0100

    jquery

--- public/src/plantae/index.php
+++ public/src/plantae/index.php
@@ -9,7 +9,7 @@
     <meta name="author" content="Maxime Princelle">
 
     <!-- Scripts -->
-    <script src="./js/jquery-3.3.1.min.js"></script>
+    <script src="./js/jquery.min.js"></script>
     <script src="./js/bootstrap.min.js"></script>
     <script src="./js/popper.min.js"></script>
     <script src="./js/angular.min.js"></script>
