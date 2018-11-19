5dd3f1a290ebcc2f35d4c5f4ca323a930dc59e0d
Maxime Princelle <princellemaxime@gmail.com>
2018-11-19 15:47:29 +0100

    Merge https://git.unistra.fr/T432-DIP18-T3-A/plantae

--- public/src/plantae/js/canva.js
+++ public/src/plantae/js/canva.js
@@ -1,26 +1,12 @@
-$(document).ready(function() {
-  $('#js-content').load('/start.html');
-
-  $('#navbar a').click(function(e) {
-    e.preventDefault();
-    $("#js-content").load(e.target.href);
-  })
-
-  $('#js-content #switch #card-body a').click(function(e) {
-    e.preventDefault();
-    $("#js-content").load(e.target.href);
-  })
-});
-
 responsiveCanvas(); //first init
 $(window).resize(function(){
   responsiveCanvas(); //every resizing
   stage.update(); //update the canvas, stage is object of easeljs
 
 });
-function responsiveCanvas(target){
-  $(canvas).each(function(e){
 
+function responsiveCanvas(){
+  $(canvas).each(function(e){
     var parentWidth = $(this).parent().outerWidth();
     var parentHeight =  $(this).parent().outerHeight();
     $(this).attr('width', parentWidth);
