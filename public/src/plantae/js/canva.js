87727f584a852dee9c0b15e49c816f22b52f388f
Maxime Princelle <princellemaxime@gmail.com>
2018-11-26 21:51:04 +0100

    Fonctions pour régler canvas

--- public/src/plantae/js/canva.js
+++ public/src/plantae/js/canva.js
@@ -1,18 +1,68 @@
-responsiveCanvas(); //first init
-$(window).resize(function(){
-  responsiveCanvas(); //every resizing
-  stage.update(); //update the canvas, stage is object of easeljs
+// canva.js
+// Created by Maxime Princelle
+//----------------------------
 
-});
+let numberFlowers;
+let biome;
+let colorFlower;
+let typeFlower;
+let imageBack;
+let imageFlower;
 
-function responsiveCanvas(){
-  $(canvas).each(function(e){
-    var parentWidth = $(this).parent().outerWidth();
-    var parentHeight =  $(this).parent().outerHeight();
+const c = document.getElementById('canvas');
+const ctx = c.getContext('2d');
 
-    $(this).attr('width', parentWidth);
-    $(this).attr('height', parentHeight);
-    console.log(parentWidth);
-  })
+function resetCanvas(option){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    if (option == "nbflowers") {
+        setBiome(biome);
+    }
+    else if (option == "changeflower") {
+        setBiome(biome);
+    }
+    else if (option == "putElements") {
+        setFlower(typeFlower, colorFlower, "keepBack");
+    }
+}
 
+function setBiome(biomeSet) {
+    biome = biomeSet;
+    imageBack = new Image();
+    imageBack.src = "./img/game/background/" + biomeSet + ".png"
+    imageBack.onload = function(){
+        ctx.drawImage(imageBack, 0, 0, canvas.width, canvas.height);
+    }
+    resetCanvas("putElements");
+}
+
+function setFlower(type, color, option) {
+    if (option != "keepBack") {
+        resetCanvas("changeflower");
+    }
+    typeFlower = type;
+    colorFlower = color;
+    imageFlower = new Image();
+    imageFlower.src = "./img/game/flower/" + type + "_" + color + ".png";
+    setFlowerNumber(numberFlowers, "keepBack");
+}
+
+function setFlowerNumber(numberFlowersSet, option) {
+    if (option != "keepBack") {
+        resetCanvas("nbflowers");
+    }
+
+    imageFlower.onload = function() {
+        numberFlowers = numberFlowersSet;
+        if (numberFlowersSet > 15) {
+          numberFlowersSet = 15;
+        }
+
+        for (let i = 0; i <= numberFlowersSet; i++) {
+          if (i < 8) {
+              ctx.drawImage(imageFlower, canvas.width - (i * 100), canvas.height - 200, 50, 100);
+          } else {
+              ctx.drawImage(imageFlower, canvas.width - ((i - 8 + 1.5) * 100), canvas.height - 150, 50, 100);
+          }
+        }
+    }
 }
