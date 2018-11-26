ca15713ef30ddd2bb0b6d3c1c9b55caf519f244c
Loïc Lehmann <l.lehmann@etu.unistra.fr>
2018-11-26 16:07:05 +0100

    Tentative de fix de adduser 2

--- public/src/plantae/php/adduser.php
+++ public/src/plantae/php/adduser.php
@@ -6,7 +6,21 @@ if ($_SERVER['REQUEST_METHOD'] != 'POST')
 }
 
 session_start();
-unset($_SESSION['message']);
+// Fichier contenant les informations de connexion à la BDD
+require_once 'bdd.php';
+
+try
+{
+    $db = new PDO(SQL_DSN, SQL_USERNAME, SQL_PASSWORD);
+}
+catch (PDOException $e)
+{
+    $_SESSION['message'] = $e->getMessage();
+    $_SESSION['message'] .= 'Erreur de connexion à la BDD';
+    header('Location: /');
+    exit;
+}
+//unset($_SESSION['message']);
 
 
 if (isset($_POST['pseudo']) && isset($_POST['login']) && isset($_POST['password']) && isset($_POST['confirm']))
@@ -22,46 +36,35 @@ if (isset($_POST['pseudo']) && isset($_POST['login']) && isset($_POST['password'
         header('Location: /');
         exit;
     }
+    else{
+        try {
+            $request = $db->prepare('INSERT INTO USERS(pseudo, login, password ) VALUES (:pseudo, :login, :password');
+            $request->bindValue(":pseudo", $pseudo);
+            $request->bindValue(':login', $login, PDO::PARAM_STR);
+            $request->bindValue(":password", password_hash($password, PASSWORD_DEFAULT));
+            $ok = $request->execute();
+        }
 
-    // Fichier contenant les informations de connexion à la BDD
-    require_once 'bdd.php';
-
-    try
-    {
-        $db = new PDO(SQL_DSN, SQL_USERNAME, SQL_PASSWORD);
-    }
-    catch (PDOException $e)
-    {
-        $_SESSION['message'] = $e->getMessage();
-        $_SESSION['message'] .= 'Erreur de connexion à la BDD';
-        header('Location: /');
-        exit;
-    }
-
-    // On essaye d'ajouter le nouvel utilisateur
-    $q = $db->prepare('INSERT INTO USERS(pseudo, login, password ) VALUES (:pseudo, :login, :password');
-    $q->bindValue(':pseudo', $pseudo, PDO::PARAM_STR);
-    $q->bindValue(':login', $login, PDO::PARAM_STR);
-    $q->bindValue(':password', password_hash($password,PASSWORD_DEFAULT), PDO::PARAM_STR);
-    $ok = $q->execute();
-    //var_dump($ok);
-
-    // Si la requête a été exécutée avec succès
-    if ( $ok )
-    {
-        $_SESSION['message'] = 'Congratulations '.$pseudo. ', Account successfully created !<br>';
-        $_SESSION['message'] .= 'Please signin to access to your account.';
-        header('Location: /');
-        exit;
-    }
-    else
-    // Si la requête a échoué, c'est que le login existe déjà
-    {
-        $_SESSION['message'] = "The login '". $login ."' still exists. Try to signin.";
-        header('Location: /');
-        exit;
+        catch( PDOException $e ) {
+            $_SESSION['message'] = $e->getMessage();
+            header("Location: signup.php");
+            exit;
+        }
+        if($ok){
+            $_SESSION['message'] = 'Congratulations '.$pseudo. ', Account successfully created !<br>';
+            $_SESSION['message'] .= 'Please signin to access to your account.';
+            header('Location: /');
+            exit;
+        }
+        else{
+            $_SESSION['message'] = "The login '". $login ."' still exists. Try to signin.";
+            header('Location: /');
+            exit;
+        }
     }
 }
-
-header('Location: /');
-exit;
+else{
+    $_SESSION['message'] = 'Champs non renseigné';
+    header('Location: /');
+    exit;
+}
