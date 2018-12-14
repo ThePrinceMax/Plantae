dded35aa1386ab302311956b0ef5606b67486b2e
Princelle Maxime <maxime.princelle@etu.unistra.fr>
2018-12-14 11:26:44 +0100

    Mettre à jour public/src/plantae/herbier.php

--- public/src/plantae/herbier.php
+++ public/src/plantae/herbier.php
@@ -1,32 +1,34 @@
 <section id="main-content">
+	<main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
 			<div class="card-deck" id="switch">
 				<div class="card">
-					<img class="card-img-left" src="./img/solo.jpg" alt="Jeu solo">
+					<img class="card-img-top" src="./img/solo.jpg" alt="Jeu solo">
 					<div class="card-body">
 						<h5 class="card-title">Solo</h5>
 						<p class="card-text">Le mode solo vous permet de jouer face à votre ordinateur. <br /> Le but est simple : être le dernier à mourrir.</p>
 					</div>
 				</div>
 				<div class="card">
-					<img class="card-img-left" src="./img/onsite.jpg" alt="Jeu OnSite - 1v1">
+					<img class="card-img-top" src="./img/onsite.jpg" alt="Jeu OnSite - 1v1">
 					<div class="card-body">
 						<h5 class="card-title">OnSite - 1v1</h5>
 						<p class="card-text">Le mode OnSite 1v1 vous permet de jouer avec un ami sur un seul ordinateur. <br />La partie se déroule en tour par tour et prends fin dès que l'un des joueurs meurt.</p>
 					</div>
 				</div>
 				<div class="card">
-					<img class="card-img-left" src="./img/lan.jpg" alt="Jeu OnLine - 1v1">
+					<img class="card-img-top" src="./img/lan.jpg" alt="Jeu OnLine - 1v1">
 					<div class="card-body">
 						<h5 class="card-title">OnLine - 1v1</h5>
 						<p class="card-text">Le mode OnLine 1v1 vous permet de jouer avec un ami mais cette fois-ci via Internet.<br />La partie se déroule en tour par tour et prends fin dès que l'un des joueurs meurt.<br>Créez ou rejoignez un salon pour pouvoir jouer.</p>
 					</div>
 				</div>
 				<div class="card">
-					<img class="card-img-left" src="./img/quizz.jpg" alt="Quizz">
+					<img class="card-img-top" src="./img/quizz.jpg" alt="Quizz">
 					<div class="card-body">
 						<h5 class="card-title">Quizz</h5>
 						<p class="card-text">Dans ce quizz, des questions vous seront posées sur plusieurs termes autour de la Botanique. <br />Répondez juste et gagnez des points afin de pouvoir acheter des bonus lors de vos parties.</p>
 					</div>
 				</div>
 			</div>
+	</main>
 </section>
