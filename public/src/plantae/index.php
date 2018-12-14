00f4e2e58f971244f2dc5dc62abafc58c4540d9a
Maxime Princelle <maximeprincelle@MacBook-Pro-de-Maxime-P.local>
2018-12-14 11:10:09 +0100

    Adding Popovers for options

--- public/src/plantae/index.php
+++ public/src/plantae/index.php
@@ -10,8 +10,8 @@
 
     <!-- Scripts -->
     <script src="./js/jquery.min.js"></script>
-    <script src="./js/bootstrap.min.js"></script>
     <script src="./js/popper.min.js"></script>
+    <script src="./js/bootstrap.min.js"></script>
     <script src="./js/angular.min.js"></script>
     <script src="./js/angular-route.js"></script>
     <script src="./js/loading.js"></script>
